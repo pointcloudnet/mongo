d88c1481d99f693c997c4f61bcbd62740bc66d57
jonathanroll <75393433+jonathanroll@users.noreply.github.com>
2023-10-10 22:39:02 +0000

    updating libs

---
+++ mongo-mongoose/index.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+// PORT=5000
+DATABASE_URI=mongodb://black:27017/?directConnection=true
+mongoose.connect("mongodb+srv://<username>:<password>@cluster0.eyhty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+DATABASE_NAME=mongmong
+CNXN_PARMS=[
+    directConnection=true
+    ,retryWrites=true
+    ,w=majority
+]
+SECRET=secret
+
+mongoose.connect("mongodb+srv://<username>:<password>@cluster0.eyhty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
