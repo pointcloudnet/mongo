6e3afd040a894fe355651135df7d7fd8e5c7ba76
jonathanroll <75393433+jonathanroll@users.noreply.github.com>
2023-10-11 17:33:37 +0000

    revising models

--- mongo-mongoose/index.js
+++ mongo-mongoose/index.js
@@ -1,14 +1,67 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+import Blog from './model/Blog.js';
+import User from './model/User.js';
 
 // PORT=5000
-DATABASE_URI=mongodb://black:27017/?directConnection=true
-mongoose.connect("mongodb+srv://<username>:<password>@cluster0.eyhty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-DATABASE_NAME=mongmong
-CNXN_PARMS=[
-    directConnection=true
-    ,retryWrites=true
-    ,w=majority
-]
-SECRET=secret
+let db_host = 'black';
+let db_port = 27017;
+let db_name = 'mongmong';
+// let db_uri = 'mongodb://black:27017/?directConnection=true';
+let db_uri = `mongodb://${db_host}:${db_port}/${db_name}`;
+// mongoose.connect("mongodb+srv://<username>:<password>@cluster0.eyhty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+let conn_parms=[
+    'directConnection=true'
+    ,'retryWrites=true'
+    ,'w=majority'
+];
+let conn_str = `${db_uri}` + '?' + conn_parms.join('&');
+// SECRET=secret
+mongoose.connect(conn_str);
 
-mongoose.connect("mongodb+srv://<username>:<password>@cluster0.eyhty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+// Create a new blog post object - Method 1
+/*
+const article = new Blog({
+    title: 'Awesome Post!',
+    slug: 'awesome-post',
+    published: true,
+    content: 'This is the best post ever',
+    tags: ['featured', 'announcement'],
+  });
+
+
+// Insert the article in our MongoDB database
+await article.save();
+
+
+// Find a single blog post
+const firstArticle = await Blog.findOne({});
+console.log(firstArticle);
+
+// Create a new blog post object - Method 2
+// Create a new blog post and insert into database
+let article2 = await Blog.create({
+    title: 'Awesome Post!',
+    slug: 'awesome-post',
+    published: true,
+    content: 'This is the best post ever',
+    tags: ['featured', 'announcement'],
+  });
+  
+  console.log(article);
+*/
+  let user = await User.create({
+    name: 'Jesse Hall'
+    ,email: 'jesse@email.com'
+  });
+
+let article = await Blog.create({
+    title: 'Awesome Post!',
+    slug: 'awesome-post',
+    author: user._id,
+    published: true,
+    content: 'This is the best post ever',
+    tags: ['featured', 'announcement'],
+  });
+
+article = await Blog.findOne({ title: "Awesome Post!" }).populate("author");
+console.log(article);
